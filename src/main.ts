2f84d9b6447afbd9e24938e0b85e8e0f37068954
Dominic Prior <dominicprior42@gmail.com>
2026-04-25 15:17:40 +0100

    Replace the innerHTML

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,19 @@ import './style.css'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
-
-<div class="ticks"></div>
-
-<div class="ticks"></div>
-<section id="spacer"></section>
+    <h2>Cliff Walking Reinforcement Learning</h2>
+    <div class="controls">
+        Algorithm: 
+        <select id="algoSelect">
+            <option value="qlearning">Q-Learning (Off-policy)</option>
+            <option value="sarsa">SARSA (On-policy)</option>
+            <option value="montecarlo">Monte Carlo (Every-visit)</option>
+        </select>
+        <button onclick="resetSim()">Reset & Run</button>
+        <div class="stats" id="stats">Episode: 0 | Steps: 0 | Reward: 0</div>
+        <small>Blue = Agent | Red = Cliff | Green = Goal | Yellow arrows = Policy</small>
+    </div>
+    <canvas id="gridCanvas" width="600" height="240"></canvas>
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
